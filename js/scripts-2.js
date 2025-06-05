1c286afcd23468979c47f6518f3202c68e1fe3b6
kadishce2 <kadishce2@gmail.com>
2025-06-05 09:52:26 -0400

    file structure and cover

--- js/scripts-2.js
+++ js/scripts-2.js
@@ -1,9 +1,10 @@
 const images = [
   'img/findlay-a-004.jpg',
-  'img/music-retreat-001.jpg',
+  'img/tpcc-001.png',
+  'img/shelter-002.jpg',
   'img/riyadh-001.jpg',
-  'img/fis-001.jpg',
-  'img/lyons-001.jpg'
+  'img/lyons-001.jpg',
+  'img/findlay-s-001.jpg'
 ];
 
 let currentIndex = 0;
@@ -19,5 +20,5 @@ function changeBackground() {
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector('.slideshow');
   if (body) body.style.backgroundImage = `url('${images[0]}')`;
-  setInterval(changeBackground, 12000); // change every 5 seconds
+  setInterval(changeBackground, 6000); // change every 5 seconds
 });
